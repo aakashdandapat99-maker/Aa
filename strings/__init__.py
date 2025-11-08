8398d6c1b4f2f4f3e42339585e4f046ea5decd3e
It's Me Vishal <vishalkurrey950+1@gmail.com>
2025-11-08 22:44:07 +0530

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -1,34 +1,34 @@
 import os
-from typing import List
-
 import yaml
 
 languages = {}
 languages_present = {}
 
-
 def get_string(lang: str):
-    return languages[lang]
+    return languages.get(lang, languages.get("en", {}))
 
+lang_path = "./strings/langs/"
 
-for filename in os.listdir(r"./strings/langs/"):
+for filename in os.listdir(lang_path):
     if "en" not in languages:
-        languages["en"] = yaml.safe_load(
-            open(r"./strings/langs/en.yml", encoding="utf8")
-        )
-        languages_present["en"] = languages["en"]["name"]
+        with open(os.path.join(lang_path, "en.yml"), encoding="utf8") as f:
+            languages["en"] = yaml.safe_load(f)
+        languages_present["en"] = languages["en"].get("name", "English")
+
     if filename.endswith(".yml"):
         language_name = filename[:-4]
         if language_name == "en":
             continue
-        languages[language_name] = yaml.safe_load(
-            open(r"./strings/langs/" + filename, encoding="utf8")
-        )
+        with open(os.path.join(lang_path, filename), encoding="utf8") as f:
+            languages[language_name] = yaml.safe_load(f)
+
+        # Fill missing keys from English
         for item in languages["en"]:
             if item not in languages[language_name]:
                 languages[language_name][item] = languages["en"][item]
-    try:
-        languages_present[language_name] = languages[language_name]["name"]
-    except:
-        print("There is some issue with the language file inside bot.")
-        exit()
+
+        # Safely set display name
+        lang_display = languages[language_name].get("name", language_name)
+        languages_present[language_name] = lang_display
+
+print(f"[INFO] Loaded {len(languages_present)} languages: {', '.join(languages_present.keys())}")
